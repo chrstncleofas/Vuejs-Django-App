f46caf896389e61c28ed4d422a4f6cc9d36928a7
Christian Q. Cleofas <cleofastiano27@gmail.com>
2024-01-14 23:02:57 +0800

    NO_TICKET - Add some text and revise in Home.vue file

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="jumbotron mt-4">
-        <h1 class="display-4">Hello! Welcome to our Employee Management Portal</h1>
+        <h1 class="display-4">Hello! Everyone <br> Welcome to our Employee Management Portal</h1>
         <p class="lead">
             This is a simple employee application, made by Vuejs CDN and Django Rest-API Framework && Poetry Package Management,
             This allow you to (GET, CREATE, EDIT, DELETE) your details.
